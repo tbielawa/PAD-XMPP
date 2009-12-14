dd5af45cd965b5dec475811b4cf2427e0ead0bf1
Tim 'Shaggy' Bielawa <timbielawa@gmail.com>
2009-12-14 07:22:23 -0500

    Project 2, a simple interactive shell.

---
+++ tests/TCP_simple_shell/simple_shell.erl
@@ -0,0 +1,8 @@
+%%% File    : simple_shell.erl
+%%% Author  : Tim Bielawa <timbielawa@gmail.com>
+%%% Description : A simple interactive shell server. Connect over telnet.
+%%% Created : 14 Dec 2009 by Tim Bielawa <timbielawa@gmail.com>
+
+-module(simple_shell).
+
+
