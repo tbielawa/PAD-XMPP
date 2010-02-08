91fd06ca546076fd72aed176d3c75beeddbabb71
Tim 'Shaggy' Bielawa <timbielawa@gmail.com>
2010-02-08 05:50:16 -0500

    Committing licensed version of gen_listener_tcp

--- src/gen_listener_tcp.erl
+++ src/gen_listener_tcp.erl
@@ -1,3 +1,40 @@
+%% 
+%% Copyright (c) 2008-2010, Essien Ita Essien
+%% All rights reserved.
+%% 
+%% Redistribution and use in source and binary forms, 
+%% with or without modification, are permitted 
+%% provided that the following conditions are met:
+%%
+%%    * Redistributions of source code must retain the 
+%%      above copyright notice, this list of conditions 
+%%      and the following disclaimer.
+%%    * Redistributions in binary form must reproduce 
+%%      the above copyright notice, this list of 
+%%      conditions and the following disclaimer in the 
+%%      documentation and/or other materials provided with 
+%%      the distribution.
+%%    * Neither the name "JsonEvents" nor the names of its 
+%%      contributors may be used to endorse or promote 
+%%      products derived from this software without 
+%%      specific prior written permission.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+%% CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED 
+%% WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+%% WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+%% PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+%% HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+%% INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+%% (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+%% GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+%% BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+%% LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+%% (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
+%% THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+%% OF SUCH DAMAGE.
+%% 
+
 -module(gen_listener_tcp).
 -behaviour(gen_server).
 
@@ -19,6 +56,7 @@
 	 abcast/3,
 	 reply/2
 	]).
+
 
 -export([
 	 init/1,
@@ -46,7 +84,10 @@ behaviour_info(callbacks) ->
      {handle_info, 2},
      {terminate, 2},
      {code_change, 3}
-    ].
+    ];
+
+behaviour_info(_Other) ->
+    undefined.
 
 
 start_link(Name, Module, InitArgs, Options) ->
@@ -95,8 +136,6 @@ reply(Client, Reply) ->
 
 init([{'__gen_listener_tcp_mod', Module} | InitArgs]) ->
     process_flag(trap_exit, true),
-
-    error_logger:info_msg("About to call gen_tcp:listen()"),
 
     case Module:init(InitArgs) of
         {ok, {Port, Options}, ModState} ->
